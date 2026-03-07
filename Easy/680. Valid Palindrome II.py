6022f4a0972adfc915193c449169808d89184e73
Sanjit-Jeevanand <75569049+Sanjit-Jeevanand@users.noreply.github.com>
2026-03-07 10:56:07 +0000

    S9 Q2

---
+++ Easy/680. Valid Palindrome II.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def is_pal(l,r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        n = len(s)
+        l = 0
+        r = n - 1
+        while l < r:
+            if s[l] != s[r]:
+                return is_pal(l+1, r) or is_pal(l, r-1)
+            l += 1
+            r -= 1
+        return True
+
+# 2 pointer for palindrome
+# If mismatch; check if Either are palindromes
