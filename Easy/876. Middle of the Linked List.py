34768ba318dc450764d550555f878afdc16de0c0
Sanjit-Jeevanand <75569049+Sanjit-Jeevanand@users.noreply.github.com>
2026-03-13 18:47:33 +0000

    S15 Q3

---
+++ Easy/876. Middle of the Linked List.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        sp = head
+        fp = head
+        while fp and fp.next:
+            fp = fp.next.next
+            sp = sp.next
+        return sp
+
+# The tortoise-hare technique
+# since fp is twice as fast; sp naturally lands in the middle
