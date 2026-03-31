d064732ea5d561c4a10cd0ed251e6cc20b79e233
Sanjit-Jeevanand <75569049+Sanjit-Jeevanand@users.noreply.github.com>
2026-03-31 13:32:04 +0530

    S33 Q3

---
+++ Medium/994. Rotting Oranges.py
@@ -0,0 +1,25 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        q = deque()
+        fresh = 0
+        time = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
+                    q.append((i,j))
+                elif grid[i][j] == 1:
+                    fresh += 1
+        while q and fresh > 0:
+            for _ in range(len(q)):
+                i, j = q.popleft()
+                for di, dj in [(1, 0), (-1, 0), (0,1), (0,-1)]:
+                    ni, nj = i + di, j + dj
+                    if 0 <= ni < m and 0 <= nj < n and grid[ni][nj] == 1:
+                        grid[ni][nj] = 2
+                        fresh -= 1
+                        q.append((ni,nj))
+            time += 1
+        return time if fresh == 0 else -1
+    
+# Grid Based Graph BFS
