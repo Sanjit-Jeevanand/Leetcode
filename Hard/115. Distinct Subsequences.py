667326f44fcc1909fb55c30be69c4f8b57230425
Sanjit-Jeevanand <75569049+Sanjit-Jeevanand@users.noreply.github.com>
2026-05-07 16:50:53 +0100

    S47 Q2

---
+++ Hard/115. Distinct Subsequences.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        l1, l2 = len(s)+1, len(t)+1
+        dp = [[0]*l2 for _ in range(l1)]
+        for i in range(l1): dp[i][0] = 1
+        for i in range(1,l1):
+            for j in range(1,l2):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        return dp[-1][-1]
