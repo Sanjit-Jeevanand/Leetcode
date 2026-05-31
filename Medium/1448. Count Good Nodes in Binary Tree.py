f77f652db54c479914d78a0e014516d6cc32b97e
Sanjit-Jeevanand <75569049+Sanjit-Jeevanand@users.noreply.github.com>
2026-05-31 19:05:36 +0100

    E11 Q1

---
+++ Medium/1448. Count Good Nodes in Binary Tree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        ans = 0
+        def dfs(node,maxi):
+            nonlocal ans
+            if not node:
+                return
+            maxi = max(maxi,node.val)
+            left = dfs(node.left, maxi)
+            right = dfs(node.right, maxi)
+            if node.val >= maxi:
+                ans += 1
+            return
+        dfs(root, root.val)
+        return ans
