83590681a630d52e8de220a6a5defb19414bdd9d
Sanjit-Jeevanand <75569049+Sanjit-Jeevanand@users.noreply.github.com>
2026-06-18 22:20:34 +0100

    S55 Q4

---
+++ Hard/123. Best Time to Buy and Sell Stock III.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        memo = {}
+        n = len(prices)
+        def solve(i,h,l):
+            if i == n: return 0
+            if (i,h,l) in memo: return memo[(i,h,l)]
+            if l == 2:
+                memo[(i,h,l)] = solve(i+1,h,l)
+            else:
+                if h:
+                    memo[(i,h,l)] = max(solve(i+1,h,l), solve(i+1,0,l+1) + prices[i])
+                else:
+                    memo[(i,h,l)] = max(solve(i+1,h,l), solve(i+1,1,l) - prices[i])
+            return memo[(i,h,l)]
+        return solve(0,0,0)
